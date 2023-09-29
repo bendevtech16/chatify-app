c9917befdcb35cb8bd2f3cb83e87f917c4639900
benjamin OHANDJA <bendevtech16@gmail.com>
2023-09-29 10:25:06 +0100

    a
    w

--- chat-app/src/app/app-routing.module.ts
+++ chat-app/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './webComponents/login/admin/admin.component';
 import { ChatAgentComponent } from './webComponents/login/chat-agent/chat-agent.component';
+import { IndexComponent } from './webComponents/landingPage/index/index.component';
 
 const routes: Routes = [
   {path:"admin", component: AdminComponent},
-  {path:"", component: AdminComponent},
-  {path:"chatAgent", component: ChatAgentComponent}
+  {path:"", component: IndexComponent},
+  {path:"chatAgent", component: ChatAgentComponent},
+  {path: "index", component: IndexComponent}
 ];
 
 @NgModule({
--- chat-app/src/app/app.module.ts
+++ chat-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './webComponents/login/admin/admin.component';
 import { ChatAgentComponent } from './webComponents/login/chat-agent/chat-agent.component';
+import { IndexComponent } from './webComponents/landingPage/index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
     ChatAgentComponent,
+    IndexComponent,
  
   ],
   imports: [
